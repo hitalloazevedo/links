370078f43cd66f773ce4cbb35160aa798563bc79
HitalloAzevedo <azevedohitallo@gmail.com>
2024-02-23 11:09:32 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const btnSwitch = document.querySelector('.switch button')
+const image = document.querySelector('.profile img')
+const mode = document.querySelector('html')
+
+btnSwitch.addEventListener('click', () => {
+    mode.classList.toggle('light')
+
+    if (mode.classList.contains('light')) {
+        image.setAttribute('src', './assets/avatar-light.png')
+    } else {
+        image.setAttribute('src', '/assets/avatar.png')
+    }
+})
